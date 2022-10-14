3ce761d92ff0531db0af2450f08ce12442cc2a38
Akbarov Jasurbek <akbarovjasurbek28@gmail.com>
2022-10-14 19:33:48 +0900

    all right'

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the capital city of South Korea?",
+    answer:"Seoul",
+    options: [
+      "Baku",
+      "Tashkent",
+      "Seoul",
+      "Madrid",
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the capital city of Australia?",
+    answer:"Canberra",
+    options: [
+      "Bali",
+      "Canberra",
+      "Kuala Lumpur",
+      "Shanghai",
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the capital city of Canada?",
+    answer:"Ottawa",
+    options: [
+      "Toronto",
+      "Tashkent",
+      "Ottawa",
+      "Rome",
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is the capital city of Italy?",
+    answer:"Rome",
+    options: [
+      "Yishun",
+      "California",
+      "Singapore",
+      "Rome",
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the capital city of Singapore?",
+    answer:"Singapore",
+    options: [
+      "Baku",
+      "Washington",
+      "Singapore",
+      "Madrid",
+    ]
+  },
+]
